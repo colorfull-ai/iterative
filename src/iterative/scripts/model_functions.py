ea4aa32086cc6ea82e8aab579828833c479b0184
Ben Garrard <bgarrard@bashful.ai>
2023-11-26 13:44:58 -0600

    added * imports for models and endpoints with dedents

--- src/iterative/scripts/model_functions.py
+++ src/iterative/scripts/model_functions.py
@@ -67,12 +67,14 @@ def generate_model(entity_name: str, model_generation_path: Optional[str] = None
     class_name = entity_name.title().replace('_', '')
     file_name = f"{class_name}.py"
     file_path = os.path.join(model_folder, file_name)
-    model_content = f"""from iterative import IterativeModel
+    model_content = _dedent(f"""from iterative import IterativeModel
+                        from typing import *     
+                        from models import *  # Reduce the imports for better performance at a later time
 
 class {class_name}(IterativeModel):
     _collection_name = "{class_name}"
     # TODO: Add fields here
-"""
+""")
 
     # Write the model class to the file, overwriting any existing file
     with open(file_path, 'w') as file:
@@ -81,24 +83,29 @@ class {class_name}(IterativeModel):
     print(f"Model {class_name} created at {file_path}")
 
 
-def add_property_to_model(entity_name: str, property_name: str, property_type: str, model_generation_path: str = None):
+def add_property_to_model(entity_name: str, property_name: str, property_type: str, model_generation_path: Optional[str] = None):
     """
     Add a new property to an existing nosql_yorm model.
 
+    Args:
+        entity_name: Name of the entity to which the property should be added.
+        property_name: Name of the property to be added.
+        property_type: The python data type of the property a primitive or Typing type.
+        model_generation_path: Path to the directory containing the model file.
     """
     model_folder = model_generation_path or os.getcwd()
-    file_name = f"{entity_name}.py".lower()
-    file_path = os.path.join(model_folder, file_name)
+    file_name = f"{entity_name}.py"
+    file_path = os.path.join(model_folder, "models", file_name)
 
     if not os.path.exists(file_path):
         print(f"Model file {file_path} does not exist.")
-        return
+        raise FileNotFoundError(f"Model file {file_path} does not exist.")
 
     new_property_line = f"    {property_name}: Optional[{property_type}] = None\n"
 
     with open(file_path, 'r+') as file:
         lines = file.readlines()
-        insert_line_index = next((i for i, line in enumerate(lines) if 'collection_name' in line), None)
+        insert_line_index = next((i for i, line in enumerate(lines) if '_collection_name' in line), None)
 
         if insert_line_index is not None:
             lines.insert(insert_line_index + 1, new_property_line)
@@ -110,13 +117,16 @@ def add_property_to_model(entity_name: str, property_name: str, property_type: s
 
     print(f"Property {property_name} added to model {entity_name} at {file_path}")
 
-
-
-def edit_property_in_model(entity_name: str, property_name: str, new_type: str, model_generation_path: str = None):
+def edit_property_in_model(entity_name: str, property_name: str, new_type: str, model_generation_path: Optional[str] = None):
     """
     Edit an existing property in a nosql_yorm model.
-    """
 
+    Args:
+        entity_name: Name of the entity to which the property belongs.
+        property_name: Name of the property to be edited.
+        new_type: The new python data type of the property a primitive or Typing type.
+        model_generation_path: Path to the directory containing the model file.
+    """
     model_folder = model_generation_path or os.getcwd()
     file_name = f"{entity_name}.py".lower()
     file_path = os.path.join(model_folder, file_name)
@@ -130,7 +140,7 @@ def edit_property_in_model(entity_name: str, property_name: str, new_type: str, 
         property_line_index = next((i for i, line in enumerate(lines) if property_name + ":" in line), None)
 
         if property_line_index is not None:
-            lines[property_line_index] = f"{property_name}: Optional[{new_type}] = None\n"
+            lines[property_line_index] = f"    {property_name}: Optional[{new_type}] = None\n"
             file.seek(0)
             file.writelines(lines)
             file.truncate()
@@ -138,3 +148,4 @@ def edit_property_in_model(entity_name: str, property_name: str, new_type: str, 
             print(f"Property {property_name} not found in {file_path}. Cannot edit property.")
 
     print(f"Property {property_name} edited in model {entity_name} at {file_path}")
+
