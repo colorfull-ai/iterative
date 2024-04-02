2c6df5f9e2bd567c5919ddf06f88a82332d70bb9
Ben Garrard <b2gdevs@gmail.com>
2024-04-01 20:56:30 -0500

    removed cache references and yorm references

--- src/iterative/service/project_management/templates/init/project_with_entry_template/main.py
+++ src/iterative/service/project_management/templates/init/project_with_entry_template/main.py
@@ -1,7 +1,6 @@
 
-# from nosql_yorm.cache import cache_handler
-from iterative.config import Config as IterativeConfig, set_config, get_config
-from iterative import start_app, cache, IterativeModel
+from iterative.config import Config as IterativeConfig, set_config
+from iterative import start_app, IterativeModel
 
 iterative_config = IterativeConfig(user_config_path="config.yaml")
 set_config(iterative_config)
@@ -13,7 +12,6 @@ user = User(id="1", yolo="John")
 user.save()
 
 assert User.get_by_id("1").yolo == "John"
-assert cache.get_document("Users", "1")["yolo"] == "John"
 
 if __name__ == "__main__":
     start_app()
