f4714a5008f7b1ee297dcb1c6334a350d5034b1f
bgarrard <b2gdevs@gmail.com>
2025-03-30 15:58:32 -0500

    fix: improve test_function documentation

--- src/iterative/test_version.py
+++ src/iterative/test_version.py
@@ -1,3 +1,10 @@
 def test_function(required_param: str):
-    """This is a test function to verify version bumping"""
+    """This is a test function to verify version bumping
+    
+    Args:
+        required_param (str): The parameter to include in the greeting
+        
+    Returns:
+        str: A greeting message including the provided parameter
+    """
     return f"Hello, {required_param}!" 
